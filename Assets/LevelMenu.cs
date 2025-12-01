3f15c3dda1658731c2d708fb9266ed670f3f09cd
mohamed moussa <mohamedmoussa97@hotmail.com>
2025-12-01 11:02:43 +0100

    Fix menu bug in LevelMenu.cs

--- Assets/LevelMenu.cs
+++ Assets/LevelMenu.cs
@@ -3,6 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class LevelMenu : MonoBehaviour
 {
+    void Start()
+    {
+        // Ensure cursor is visible and unlocked for the menu
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    void Update()
+    {
+        // Force unlock in case another script (like MouseLook) tries to lock it
+        if (Cursor.lockState != CursorLockMode.None)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
     public void LoadLevel1()
     {
         SceneManager.LoadScene("Level_1");
